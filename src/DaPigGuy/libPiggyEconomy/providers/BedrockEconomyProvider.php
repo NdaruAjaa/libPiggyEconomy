b7016865a9954778c6bcc02e27e8748b388159a4
NdaruAjaa <120300909+NdaruAjaa@users.noreply.github.com>
2023-12-12 14:22:54 +0700

    Update BedrockEconomyProvider.php

--- src/DaPigGuy/libPiggyEconomy/providers/BedrockEconomyProvider.php
+++ src/DaPigGuy/libPiggyEconomy/providers/BedrockEconomyProvider.php
@@ -4,18 +4,20 @@ declare(strict_types=1);
 
 namespace DaPigGuy\libPiggyEconomy\providers;
 
-use cooldogedev\BedrockEconomy\api\legacy\ClosureContext;
+use cooldogedev\BedrockEconomy\api\type\ClosureAPI;
 use cooldogedev\BedrockEconomy\BedrockEconomy;
 use cooldogedev\BedrockEconomy\api\BedrockEconomyAPI;
-use cooldogedev\BedrockEconomy\api\version\LegacyBEAPI;
-use cooldogedev\BedrockEconomy\currency\CurrencyManager;
+use cooldogedev\BedrockEconomy\currency\Currency;
+use cooldogedev\BedrockEconomy\database\constant\Search;
+use cooldogedev\BedrockEconomy\database\exception\RecordNotFoundException;
+use cooldogedev\libSQL\exception\SQLException;
 use pocketmine\player\Player;
 use pocketmine\Server;
 
 class BedrockEconomyProvider extends EconomyProvider
 {
-    private LegacyBEAPI $api;
-    private CurrencyManager $currency;
+    private ClosureAPI $api;
+    private Currency $currency;
 
     public static function checkDependencies(): bool
     {
@@ -24,32 +26,56 @@ class BedrockEconomyProvider extends EconomyProvider
 
     public function __construct()
     {
-        $this->api = BedrockEconomyAPI::legacy();
-        $this->currency = BedrockEconomy::getInstance()->getCurrencyManager();
+        $this->api = BedrockEconomyAPI::CLOSURE();
+        $this->currency = BedrockEconomy::getInstance()->getCurrency();
     }
 
     public function getMonetaryUnit(): string
     {
-        return $this->currency->getSymbol();
+        return $this->currency->symbol;
     }
 
     public function getMoney(Player $player, callable $callback): void
     {
-        $this->api->getPlayerBalance($player->getName(), ClosureContext::create(fn(?int $balance) => $callback($balance ?? $this->currency->getDefaultBalance())));
+        $this->api->get(Search::EMPTY, $player->getName(), static fn(array $result) => $callback($result["amount"] ?? $this->currency->defaultAmount), static function(SQLException $exception): void {
+            if ($exception instanceof RecordNotFoundException) {
+                echo "Record not found";
+                return;
+            }
+            echo $exception->getMessage();
+        });
     }
 
     public function giveMoney(Player $player, float $amount, ?callable $callback = null): void
     {
-        $this->api->addToPlayerBalance($player->getName(), (int)$amount, $callback ? ClosureContext::create($callback) : null);
+        $this->api->add(Search::EMPTY, $player->getName(), (int) $amount, 0, $callback ?? static fn() => null, static function(SQLException $exception): void {
+            if ($exception instanceof RecordNotFoundException) {
+                echo "Account not found";
+                return;
+            }
+            echo $exception->getMessage();
+        });
     }
 
     public function takeMoney(Player $player, float $amount, ?callable $callback = null): void
     {
-        $this->api->subtractFromPlayerBalance($player->getName(), (int)$amount, $callback ? ClosureContext::create($callback) : null);
+        $this->api->subtract(Search::EMPTY, $player->getName(), (int) $amount, 0, $callback ?? static fn() => null, static function(SQLException $exception): void {
+            if ($exception instanceof RecordNotFoundException) {
+                echo "Account not found";
+                return;
+            }
+            echo $exception->getMessage();
+        });
     }
 
     public function setMoney(Player $player, float $amount, ?callable $callback = null): void
     {
-        $this->api->setPlayerBalance($player->getName(), (int)$amount, $callback ? ClosureContext::create($callback) : null);
+        $this->api->set(Search::EMPTY, $player->getName(), (int) $amount, 0, $callback ?? static fn() => null, static function(SQLException $exception): void {
+            if ($exception instanceof RecordNotFoundException) {
+                echo "Account not found";
+                return;
+            }
+            echo $exception->getMessage();
+        });
     }
 }
